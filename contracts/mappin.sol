3b965e4c8ed05b52acdf55c9e7719dab50ed20d8
therevealer <officialnfluence@gmail.com>
2023-07-04 00:09:28 +0100

    Basics of Mapping

--- contracts/mappin.sol
+++ contracts/mappin.sol
@@ -1,2 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+contract Mapping {
+
+    mapping(uint => bool) public myMapping;
+    mapping(string => bool) public myString;
+
+    
+}
